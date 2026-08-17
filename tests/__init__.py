016bf521f040eb1e93df626cc3007e828da83af5
Crysta1221 <takuyakojim@outlook.jp>
2026-08-17 14:29:17 +0900

    :bug: fix sonarqube warning issues

--- tests/__init__.py
+++ tests/__init__.py
@@ -7,13 +7,13 @@ class TestCSVPrinter(unittest.TestCase):
         printer = CSVPrinter("example.csv")
         line = printer.read_lines()
         print(line)
-        self.assertEqual(3, len(line))
+        self.assertEqual(len(line), 3, "Unexpected number of lines")
 
     def test_read_cols(self):
         printer = CSVPrinter("example.csv")
         col = printer.read_cols()
         print(col)
-        self.assertEqual(4, len(col))
+        self.assertEqual(len(col), 4, "Unexpected number of columns")
 
     def test_file_not_found(self):
         printer = CSVPrinter("example1.csv")
